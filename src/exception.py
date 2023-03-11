f4d96eb6324e52b302f9ac14543efa1f19963d15
ramprasathk07 <ramprasath61200@gmail.com>
2023-03-11 19:59:07 +0530

    debug

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,9 @@
 import sys
-
+import logging
 def error_msg_details(error,error_details:sys):
-    _,_,exc_tb=error.details.exc_info()
+    _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_msg="Error at {2} \n Error: {0} at line {1}".format(error_details,error_details.tb_lineno,file_name)
+    error_msg="Error at {2} \n Error: {0} at line {1}".format(error_details,exc_tb.tb_lineno,file_name)
     return error_msg
 
 class CustomException(Exception):
@@ -14,3 +14,12 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_msg 
+
+
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("logging")
+        raise CustomException(e,sys)
+    
